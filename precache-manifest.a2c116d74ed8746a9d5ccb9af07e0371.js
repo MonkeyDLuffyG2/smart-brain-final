c7512dd67877a250585d54da47f4eb71f7a10ac0
Luis Telemaco <ltelemaco14@gmail.com>
2022-01-05 04:20:13 -0400

    Updates

---
+++ precache-manifest.a2c116d74ed8746a9d5ccb9af07e0371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a882b1595a6f8b0de7a44dc65f0586",
+    "url": "/smart-brain-final/index.html"
+  },
+  {
+    "revision": "12be71612b6325e82597",
+    "url": "/smart-brain-final/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "5b2f5436159aee05fc88",
+    "url": "/smart-brain-final/static/css/main.9eadf9b0.chunk.css"
+  },
+  {
+    "revision": "12be71612b6325e82597",
+    "url": "/smart-brain-final/static/js/2.c3f17041.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/smart-brain-final/static/js/2.c3f17041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c3943e1125343dfd77",
+    "url": "/smart-brain-final/static/js/3.cd3bc51e.chunk.js"
+  },
+  {
+    "revision": "5b2f5436159aee05fc88",
+    "url": "/smart-brain-final/static/js/main.7c1a6df4.chunk.js"
+  },
+  {
+    "revision": "785477633696a2236524",
+    "url": "/smart-brain-final/static/js/runtime-main.288cd254.js"
+  }
+]);
